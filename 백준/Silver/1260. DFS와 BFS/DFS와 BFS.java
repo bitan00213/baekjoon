f416eeb10bb6d190e394f43efd230903afe8157e
bitan00213 <89769906+bitan00213@users.noreply.github.com>
2022-07-24 22:34:30 +0900

    [Silver II] Title: DFS와 BFS, Time: 268 ms, Memory: 23068 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,95 @@
+/*
+ * https://www.acmicpc.net/problem/1260
+ * http://www.tcpschool.com/java/java_collectionFramework_stackQueue 참조
+ * */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int[][] numArr;
+	public static boolean[] visit;
+	public static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		 sb = new StringBuilder();
+		
+		int N  = Integer.parseInt(st.nextToken());
+		int M  = Integer.parseInt(st.nextToken());
+		int V  = Integer.parseInt(st.nextToken());
+		
+		numArr = new int[N+1][N+1];
+		visit = new boolean[N+1];
+		
+		StringTokenizer str;
+		for(int i = 0; i<M; i++) {
+			str = new StringTokenizer(br.readLine());
+			int tempN = Integer.parseInt(str.nextToken());
+			int tempM = Integer.parseInt(str.nextToken());
+			numArr[tempN][tempM] = 1; //없으면 0
+			numArr[tempM][tempN] = 1; //큰 수에서 작은 수 탐색도 가능해야 함.
+		};
+		DFS2(N,M,V);
+		visit = new boolean[N+1];
+		sb.append('\n');
+		BFS(N,M,V);
+		System.out.println(sb);
+		
+	};
+	
+	public static void DFS(int N, int M, int V) {
+		visit[V] = true;
+		sb.append(V).append(' ');
+		for(int i = 0; i<=N; i++) {
+			if((numArr[V][i] == 1) && !visit[i]) {
+				DFS(N, M, i);
+			};
+		};
+	};
+	
+	public static void DFS2(int N, int M, int V) { 
+		visit[V] = true;
+		sb.append(V).append(' ');
+		Deque<Integer> stack = new LinkedList<Integer>();
+		stack.push(V);
+		while(!stack.isEmpty()) {
+			int tempNum = stack.peek();
+			boolean temp = false;
+			for(int i = 0; i<=N; i++) {
+				if((numArr[tempNum][i] == 1) && !visit[i]) {
+					temp = true;
+					visit[i] = true;
+					sb.append(i).append(' ');
+					stack.push(i);
+					break;
+				};
+			};
+			if(!temp) {
+				stack.pop();
+			};
+		};
+	};
+	
+	public static void BFS(int N, int M, int V) {
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(V);
+		visit[V] = true;
+		sb.append(V).append(' ');
+		while(!queue.isEmpty()) {
+			int temp = queue.poll();
+			for(int i = 1; i<=N; i++) {
+				if((numArr[temp][i] == 1) && !visit[i]) {
+					queue.add(i);
+					visit[i] = true;
+					sb.append(i).append(' ');
+				};
+			};
+		};
+	};
+};
